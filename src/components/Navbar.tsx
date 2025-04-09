e323f964f2c34625a1cb2db640ad9e15c8510c6a
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-04-09 10:43:08 +0530

    Some fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,11 +24,11 @@ function Navbar() {
     router.push("/signin"); 
   };
   return (
-    <nav className="absolute top-0 left-0 w-full flex justify-between items-center px-6 py-4 bg-black/30 backdrop-blur-sm z-20">
+    <nav className="absolute top-0 left-0 w-full flex justify-between items-center gap-2 px-6 py-4 bg-black/30 backdrop-blur-sm z-20">
         <div className="text-xl font-bold text-white">RoleBase</div>
-        { session && <div className="text-xl font-bold"><span className="hover:text-purple-400 transition duration-300 cursor-pointer" onClick={()=>redirect("/user")}>Check User Details</span></div>}
+        { session && <div className="text-md font-bold"><span className="hover:text-purple-400 transition duration-300 cursor-pointer" onClick={()=>redirect("/user")}>User Details</span></div>}
         {/* Desktop menu */}
-        <ul className="md:flex gap-6 text-white text-md md:px-4">
+        <ul className="flex md:flex gap-6 text-white text-md md:px-4">
           <li className="transition duration-300 cursor-pointer bg-pink-400 px-4 py-2 rounded-md shadow-lg hover:text-black">
           {!session ? (
               <button className="cursor-pointer" onClick={() =>redirect("/signin")}>
